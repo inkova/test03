954d21d4c53d6b7eaa64ba22d21b3a76bbbc47ef
Nataliya Inkova <inkovan@students.programming1189.ru>
2017-09-22 16:40:14 +0300

    Uraaaa!!!!!

--- pascal.cpp
+++ pascal.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+int factor(int x)
+{   int y=1;
+    for( int i=1;i<=x;++i) y=y*i;
+    return y;
+}
 
 int main()
 {
@@ -13,6 +19,21 @@ int main()
     // 1 3 3 1
     // 1 4 6 4 1
     // ...
+int i;
+int res;
+cout << "Input number of lines\n";
+cin >> i;
+vector <vector <int> > v(i);
+for(int j=0;j<i; ++j){
+ for(int k=0;k<=j;++k)
+ {cout << j <<" "<< factor(j)<< " ";
+res=factor(j)/(factor(k)*factor(j-k));
+v[j].push_back(res);}}
+for(int j=0;j<i; ++j){
+ for(int k=0;k<=j;++k)
+      {cout<< v[j][k]<< " ";}
+cout << "\n";
+}
     return 0;
 }
 
